26520dba9d00ef22b7f1557d5972f324f1657d93
Salim Abubakar <samatazu@gmail.com>
2024-12-22 18:38:07 +0100

    feat: design

--- app/page.tsx
+++ app/page.tsx
@@ -2,100 +2,49 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+    <div className="flex flex-col items-center  text-[#333333] mt-10">
+      <div className=" md:w-[380px] mb-4 items-start">
+        <Image src="logo.svg" width={116} height={13} alt="logo" />
+      </div>
+      <div className=" bg-[#f3f3f3] flex flex-col gap-6 rounded-3xl p-4 md:w-[380px] mx-auto">
+        <p>
+          <span className=" font-medium">tiny spec</span>, is a design agency
+          that focuses on designing and building thoughtful & functional
+          internet software for startups with strong focus on branding, visual
+          design & quality.
+        </p>
+        <p>
+          we are available for new projects. For enquiries, contact us at{" "}
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="mailto:work@tinyspec.co"
+            className=" font-medium transition-all hover:text-[#0074F9]"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            work@tinyspec.co
           </a>
+        </p>
+      </div>
+      <div className=" bg-white border border-[#f3f3f3] flex flex-col gap-6 rounded-3xl p-4 w-[380px] mx-auto mt-3">
+        <p>
+          <span className=" font-medium">Clients:</span>
+        </p>
+        <div>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://www.bykaer.com/"
             target="_blank"
-            rel="noopener noreferrer"
+            className=" bg-[#0074F9] hover:bg-[#0091f9] text-white py-2 px-2 rounded-3xl transition-all"
           >
-            Read our docs
+            kaer.com
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className=" bg-[#f3f3f3] flex flex-col gap-6 rounded-3xl p-4 w-[380px] mx-auto relative -top-6 -z-10 ">
+        <p className=" mt-4">
+          <span className=" font-medium">Projects:</span>
+        </p>
+        <div>
+          <p>Coming soon!</p>
+        </div>
+      </div>
     </div>
   );
 }
