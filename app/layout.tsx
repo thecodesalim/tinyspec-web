579cac80ef1fb0a46e6611c5bb7fb47dd2a615b4
Salim Abubakar <samatazu@gmail.com>
2025-04-17 05:05:51 +0100

    feat: tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,6 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="b90169ea-3ea7-485b-8b4c-567d5c6dbc5a"
+        />
+      </head>
       <body className={`${inter.className}  antialiased`}>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,19 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col items-center  text-[#333333] mt-10">
-      <div className=" md:w-[380px] mb-4 items-start">
+      <div className=" w-full md:w-[380px] mb-4 ml-4 md:ml-0">
         <Image src="logo.svg" width={116} height={13} alt="logo" />
       </div>
       <div className=" bg-[#f3f3f3] flex flex-col gap-6 rounded-3xl p-4 md:w-[380px] mx-auto">
         <p>
-          <span className=" font-medium">tiny spec</span>, is a design agency
-          that focuses on designing and building thoughtful & functional
-          internet software for startups with strong focus on branding, visual
-          design & quality.
+          <span className=" font-medium">tiny spec</span> is a studio that
+          designs and develops marketing sites, online stores, and web apps.
         </p>
         <p>
           we are available for new projects. For enquiries, contact us at{" "}
           <a
             href="mailto:work@tinyspec.co"
-            className=" font-medium transition-all hover:text-[#0074F9]"
+            className="font-medium transition-all text-[#0074F9] hover:text-[#0053f9]"
           >
             work@tinyspec.co
           </a>
@@ -27,14 +25,25 @@ export default function Home() {
         <p>
           <span className=" font-medium">Clients:</span>
         </p>
-        <div>
-          <a
-            href="https://www.bykaer.com/"
-            target="_blank"
-            className=" bg-[#0074F9] hover:bg-[#0091f9] text-white py-2 px-2 rounded-3xl transition-all"
-          >
-            kaer.com
-          </a>
+        <div className=" flex gap-4">
+          <div>
+            <a
+              href="https://www.bykaer.com/"
+              target="_blank"
+              className=" bg-[#0074F9] hover:bg-[#0091f9] text-white py-2 px-2 rounded-3xl transition-all"
+            >
+              kaer.com
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.scentsbyaw.com/"
+              target="_blank"
+              className=" bg-[#0074F9] hover:bg-[#0091f9] text-white py-2 px-2 rounded-3xl transition-all"
+            >
+              scentsbyaw.com
+            </a>
+          </div>
         </div>
       </div>
       <div className=" bg-[#f3f3f3] flex flex-col gap-6 rounded-3xl p-4 w-[380px] mx-auto relative -top-6 -z-10 ">
