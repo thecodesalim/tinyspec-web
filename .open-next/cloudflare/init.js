ed4a00190aef6fc3b9f44f4e08bbad31f6308af3
thecodesalim <samatazu@gmail.com>
2025-10-01 19:44:30 +0100

    fix: preview

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1759323341381,
+    __BUILD_TIMESTAMP_MS__: 1759344187997,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
